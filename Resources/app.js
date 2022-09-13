855f058309b38a66c1efb7d2066813e551a6bde9
Naveen Raj Y <naveenrajy@yahoo.com>
2022-09-13 13:17:05 +0530

    flashanimation appended

--- Resources/app.js
+++ Resources/app.js
@@ -1,47 +1,29 @@
-/**
- * Create a new `Ti.UI.TabGroup`.
- */
-var tabGroup = Ti.UI.createTabGroup();
+var win = Ti.UI.createWindow({
+    title: 'title',
+    backgroundColor: '#fff'
+});
 
-/**
- * Add the two created tabs to the tabGroup object.
- */
-tabGroup.addTab(createTab("Tab 1", "I am Window 1", "assets/images/tab1.png"));
-tabGroup.addTab(createTab("Tab 2", "I am Window 2", "assets/images/tab2.png"));
+var box = Ti.UI.createView({
+    backgroundColor: 'red',
+    height: '100',
+    width: '100'
+});
+win.add(box);
 
-/**
- * Open the tabGroup
- */
-tabGroup.open();
-
-/**
- * Creates a new Tab and configures it.
- *
- * @param  {String} title The title used in the `Ti.UI.Tab` and it's included `Ti.UI.Window`
- * @param  {String} message The title displayed in the `Ti.UI.Label`
- * @return {String} icon The icon used in the `Ti.UI.Tab`
- */
-function createTab(title, message, icon) {
-    var win = Ti.UI.createWindow({
-        title: title,
-        backgroundColor: '#fff'
+box.addEventListener('click', function () {
+    var matrix = Ti.UI.createMatrix2D();
+    matrix = matrix.rotate(180);
+    matrix = matrix.scale(2, 2);
+    var a = Ti.UI.createAnimation({
+        transform: matrix,
+        duration: 2000,
+        autoreverse: true,
+        repeat: 3
     });
+    box.animate(a);
+});
 
-    var label = Ti.UI.createLabel({
-        text: message,
-        color: "#333",
-        font: {
-            fontSize: 20
-        }
-    });
 
-    win.add(label);
+win.add(box);
 
-    var tab = Ti.UI.createTab({
-        title: title,
-        icon: icon,
-        window: win
-    });
-
-    return tab;
-}
+win.open();
